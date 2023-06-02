f7359c6d90bac57b7de0d43c801e1203eae56138
Liviu <liviutoma946@gmail.com>
2023-06-03 01:56:24 +0300

    added main files

---
+++ player.js
@@ -0,0 +1,15 @@
+export default class Player {
+    constructor(game) {
+        this.game = game;
+        this.width = 100;
+        this.height = 91.3;
+        this.x = 0;
+        this.y = this.game.height - this.height;
+    }
+    update() {
+        
+    }
+    draw(ctx) {
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
