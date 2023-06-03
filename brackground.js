958e08943becf1e2a27ffca9076232786f063dcb
Liviu <liviutoma946@gmail.com>
2023-06-03 18:33:37 +0300

    added background module

---
+++ brackground.js
@@ -0,0 +1,18 @@
+class Layer {
+    constructor(game, width, height, speedModifier, image) {
+        this.game = game;
+        this.width = width;
+        this.height = height;
+        this.speedModifier = speedModifier;
+        this.image = image;
+        this.x = 0;
+        this.y = 0;
+    }
+    update() {
+        if (this.x < -this.width) this.x = 0;
+        else this.x -= this.game.speed * this.speedModifier;
+    }
+    draw() {
+
+    }
+}
--- player.js
+++ player.js
@@ -1,15 +1,29 @@
+import { Sitting, Running, Jumping, Falling } from "./playerState.js";
+
 export default class Player {
     constructor(game) {
+        this.image = document.getElementById('player');
+        this.frameX = 0;
+        this.frameY = 0;
+        this.maxFrame = 5;
+        this.fps = 20;
+        this.frameInterval = 1000/this.fps;
+        this.frameTimer = 0;
         this.game = game;
         this.width = 100;
         this.height = 91.3;
         this.x = 0;
-        this.y = this.game.height - this.height;
-        this.image = document.getElementById('player');
+        this.y = this.game.height - this.height - this.game.groundMargin;
+        this.vy = 0;
+        this.weight = 1;
         this.speed = 0;
         this.maxSpeed = 10;
+        this.states = [new Sitting(this), new Running(this), new Jumping(this), new Falling(this)];
+        this.currentState = this.states[0];
+        this.currentState.enter();
     }
-    update(input) {
+    update(input, deltaTime) {
+        this.currentState.handleInput(input);
         // horizontal movement
         this.x += this.speed;
         if (input.includes('ArrowRight')) this.speed = this.maxSpeed;
@@ -17,8 +31,27 @@ export default class Player {
         else this.speed = 0;
         if (this.x < 0) this.x = 0;
         if (this.x + this.width > this.game.width) this.x = this.game.width - this.width;
+        // vertical movement
+        this.y += this.vy;
+        if (!this.onGround()) this.vy += this.weight;
+        else this.vy = 0;
+        // sprite animation
+        if (this.frameTimer > this.frameInterval) {
+            this.frameTimer = 0;
+            if (this.frameX < this.maxFrame) this.frameX++;
+            else this.frameX = 0;
+        } else {
+            this.frameTimer += deltaTime;
+        }
     }
     draw(ctx) {
-        ctx.drawImage(this.image, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
+    }
+    onGround() {
+        return this.y + this.height >= this.game.height - this.game.groundMargin;
+    }
+    setState(state) {
+        this.currentState = this.states[state];
+        this.currentState.enter();
     }
 }
