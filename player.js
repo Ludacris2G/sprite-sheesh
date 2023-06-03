292f6cd97ac65d870198a1d6c96e716dfbd8deca
Liviu <liviutoma946@gmail.com>
2023-06-03 17:09:22 +0300

    added player inputs

--- player.js
+++ player.js
@@ -5,11 +5,20 @@ export default class Player {
         this.height = 91.3;
         this.x = 0;
         this.y = this.game.height - this.height;
+        this.image = document.getElementById('player');
+        this.speed = 0;
+        this.maxSpeed = 10;
     }
-    update() {
-        
+    update(input) {
+        // horizontal movement
+        this.x += this.speed;
+        if (input.includes('ArrowRight')) this.speed = this.maxSpeed;
+        else if (input.includes('ArrowLeft')) this.speed = -this.maxSpeed;
+        else this.speed = 0;
+        if (this.x < 0) this.x = 0;
+        if (this.x + this.width > this.game.width) this.x = this.game.width - this.width;
     }
     draw(ctx) {
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height);
     }
 }
